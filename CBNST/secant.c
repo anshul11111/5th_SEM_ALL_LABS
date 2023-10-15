99fe4009b80ba2453d27ab34a3b22e7cfce288aa
Anshul Rawat <anshul.rawat1122004@gmail.com>
2023-10-15 21:41:27 +0530

    updated

--- CBNST/secant.c
+++ CBNST/secant.c
@@ -2,42 +2,47 @@
 #include <math.h>
 
 #define F(x) (x * x * x - 4 * x - 9)
-#define Fu "f(x) = x^2 - 4x - 9 = 0"
-#define SECANT(x0, x1) (x1 - ((x1 - x0) / (F(x1) - F(x0))) * F(x1))
 
-int FindRoot(double x0, double x1, double *root)
+double SecantMethod(double x0, double x1, double tol)
 {
+    double x2, f0, f1;
     int itr = 0;
-    double x2, prev_x2;
-    prev_x2 = x1; // Initialize previous root value with x1 for Secant
 
     do
     {
-        x2 = SECANT(x0, x1);
-        itr++; // Increment the iteration count
-        printf("Iteration: %d value: %.4lf; For x0: %.4lf x1: %.4lf\n", itr, x2, x0, x1);
+        f0 = F(x0);
+        f1 = F(x1);
+        x2 = x1 - (f1 * (x1 - x0) / (f1 - f0));
+        printf("Iteration %d: x = %.4lf, f(x) = %.4lf\n", itr, x2, F(x2));
 
-        if (fabs(x2 - prev_x2) <= 0.0001)
-            break; // stop on: |x(n) - x(n-1)| <= 0.0001
+        if (fabs(x2 - x1) < tol)
+        {
+            printf("Converged to root: x = %.4lf\n", x2);
+            return x2;
+        }
 
-        prev_x2 = x2; // Update previous root value
-        x0 = x1;      // Update x0 with the current value of x1
-        x1 = x2;      // Update x1 with the current value of x2
-    } while (1);
+        x0 = x1;
+        x1 = x2;
+        itr++;
+    } while (itr < 100); // Maximum iterations to prevent infinite loop
 
-    *root = x2;
-    return itr;
+    printf("Method did not converge\n");
+    return NAN;
 }
 
 int main()
 {
-    int ch;
-    double x0, x1, root; // Set precision to 4 decimal places
-    printf("Enter Interval (x0, x1): ");
-    scanf("%lf %lf", &x0, &x1);
-    printf("\t\tSecant Method\n");
-    int itr = FindRoot(x0, x1, &root); // Pass root by reference
-    printf("Root of Function %s: %.4lf\n", Fu, root);
-    printf("After: %d Iterations\n", itr);
+    double x0, x1, tol;
+
+    printf("Enter initial guess x0: ");
+    scanf("%lf", &x0);
+    printf("Enter initial guess x1: ");
+    scanf("%lf", &x1);
+    printf("Enter tolerance: ");
+    scanf("%lf", &tol);
+
+    double root = SecantMethod(x0, x1, tol);
+    printf("Root: %.4lf\n", root);
+
     return 0;
 }
