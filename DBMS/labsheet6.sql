4cdeffbf2b3c77ade69d26c1c8a2fd7e5e994e77
anshul <anshul.rawat1122004@gmail.com>
2023-10-17 10:32:18 +0530

    .

---
+++ DBMS/labsheet6.sql
@@ -0,0 +1,6 @@
+--1. Logon to Oracle using your User Name.
+connect username/password
+--2. See a list of tables by using "Tab / cat" tables.
+SELECT * FROM tab; --can use cat instead of tab
+--3. Create a table "Employee" with attributes.
+CREATE TABLE Employee
